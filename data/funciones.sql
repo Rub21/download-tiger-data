489706dddcdb0cd175b0e112e843f990119fb719
Rub21 <rub2106@gmail.com>
2014-08-27 16:47:05 -0500

    Funciones

--- data/funciones.sql
+++ data/funciones.sql
@@ -1,4 +1,23 @@
-﻿--Funciones que verifica si un way se repide o no.
+﻿--Funciones para obtener GEOID
+CREATE OR REPLACE FUNCTION get_geoid(_coordinates varchar(100)) 
+RETURNS  varchar
+AS $$
+DECLARE	
+	_geoid varchar(5);
+	BEGIN
+		_geoid = (select geoid from county WHERE ST_Within(ST_GeomFromText('LINESTRING('||_coordinates||')',4326),county.geom));
+		IF(_geoid IS NULL)
+		THEN		
+		_geoid = (select geoid from county WHERE ST_DWithin(ST_GeomFromText('LINESTRING('||_coordinates||')',4326),county.geom,0.1) limit 1);
+		END IF;
+	RETURN _geoid;
+		
+	END;
+	
+$$ LANGUAGE plpgsql;
+--SELECT  fullname, ST_AsGeoJSON(geom) FROM tl_2013_29037_roads WHERE   st_within(tl_2013_29037_roads.geom ,ST_MakeEnvelope(-94.47139263153076,38.70488643696675,-94.46366786956787,38.71032837941321, 4326))
+
+--Funciones que verifica si un way se repide o no.
    CREATE OR REPLACE FUNCTION is_overlapping(_geom geometry, _fullname varchar(100), _coordinates varchar(250),_id_table varchar(5)) RETURNS Geometry AS
    $BODY$
    DECLARE 
@@ -16,6 +35,7 @@
    END
    $BODY$
    LANGUAGE 'plpgsql';
+   
 --Funcion para optener los datos
    CREATE OR REPLACE FUNCTION get_data(_coordinates varchar(250),_id_table varchar(5) )  RETURNS SETOF tl_2013_45013_roads AS
    $BODY$
@@ -36,5 +56,5 @@
    $BODY$
    LANGUAGE 'plpgsql';
    
-select fullname, ST_AsGeoJSON(geom) FROM  get_data('-81.62992000579834,39.25781137991596,-81.62306427955627,39.263568176020584','54107');
-SELECT  fullname, ST_AsGeoJSON(geom) as geometry FROM tl_2013_54107_roads WHERE   st_within(tl_2013_54107_roads.geom ,ST_MakeEnvelope(-81.62992000579834,39.25781137991596,-81.62306427955627,39.263568176020584, 4326));
+--select fullname, ST_AsGeoJSON(geom) FROM  get_data('-81.62992000579834,39.25781137991596,-81.62306427955627,39.263568176020584','54107');
+--SELECT  fullname, ST_AsGeoJSON(geom) as geometry FROM tl_2013_54107_roads WHERE   st_within(tl_2013_54107_roads.geom ,ST_MakeEnvelope(-81.62992000579834,39.25781137991596,-81.62306427955627,39.263568176020584, 4326));
